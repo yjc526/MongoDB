59fc33a8bf03e927a5ea924bbac4007bd37b62b5
yjc526 <yjc526@naver.com>
2019-11-07 12:59:56 +0900

    add disconnect.js

--- exercise.js
+++ exercise.js
@@ -1,43 +1,36 @@
 const mongoose = require("mongoose");
 
 mongoose
-    .connect("mongodb://localhost/exercise-basic", {
-        //playground DB
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log("DB에 연결 되었습니다!"))
-    .catch(error => console.error(error));
+  .connect("mongodb://localhost/exercise-basic", {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("dbconnected"));
+const schema = mongoose.Schema();
 
-
-
-const schema = mongoose.Schema()
 const Course = mongoose.model("Course", schema);
 
-async function read() {
-    const courses = await Course.find();
+Course.find({ isPublished: true })
+  .where("tags")
+  .in(["backend"])
+  .select("name author")
+  .then(result => console.log("실습1:", result));
 
-    const exam1 = await Course.find()
-        .where("isPublished").equals(true)
-        .where("tags").in(["backend"])
-        .sort("name")
-        .select("name author");
-
-    const exam2 = await Course.find()
-        .where("isPublished").equals(true)
-        .where("tags").in(["backend", "frontend"])
-        .sort("-price")
-        .select("name price");
-
-    const exam3 = await Course.find()
-        .where("name").in([/js/i])
-        .where("price").gte(15)
-
-
-    console.log("[ courses ] ", courses);
-    console.log("[ 실습1 ] ", exam1);
-    console.log("[ 실습2 ] ", exam2);
-    console.log("[ 실습3 ] ", exam3);
+async function 실습2() {
+  const result = await Course.find({
+    isPublished: true
+    // tags: { $in: ["frontend", "backend"] }
+    // tags: ["frontend", "backend"]
+  })
+    .or([{ tags: "frontend" }, { tags: "backend" }])
+    .sort("-price")
+    .select("name price");
+  console.log("실습2 :", result);
 }
+실습2();
 
-read();
+Course.find()
+  .where("price")
+  .gte(15)
+  .where("name")
+  .regex(/js/i)
+  .then(res => console.log("실습3 : ", res));
